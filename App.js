2d093792bc69f661168e25744c83bdda4830ec55
dev-gg-3004 <guptagopal.3004@gmail.com>
2025-02-05 20:02:57 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import Calculator from './src/components/screen/calculator/Calculator';
+
+
+export default function App() {
+  return  (
+     <Calculator />
+  )
+}
